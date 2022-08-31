d9c62257a7115e89a81d6ba0d284e93426b33566
Henrique Combochi <henrique.combochi@gmail.com>
2022-08-31 18:49:51 -0300

    Added homepage layout

--- lib/pages/home_page/index.dart
+++ lib/pages/home_page/index.dart
@@ -1,10 +1,107 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.grey[700],
+        appBar: AppBar(
+          backgroundColor: Colors.grey[900],
+          title: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.black,
+            ),
+            onPressed: () {},
+            child: const Text("Site oficial"),
+          ),
+          actions: [
+            Container(
+              margin: const EdgeInsets.all(8),
+              decoration: const BoxDecoration(
+                color: Colors.black,
+                shape: BoxShape.circle,
+              ),
+              child: IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.person),
+              ),
+            ),
+          ],
+        ),
+        body: Column(
+          children: [
+            Container(
+              color: Colors.grey[800],
+              padding: const EdgeInsets.all(8.0),
+              child: TabBar(
+                controller: _tabController,
+                indicator: BoxDecoration(
+                  color: Colors.grey,
+                  border: Border.all(),
+                ),
+                tabs: const [
+                  Tab(
+                    text: "Filmes",
+                  ),
+                  Tab(
+                    text: "Personagens",
+                  ),
+                  Tab(
+                    text: "Favoritos",
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: TabBarView(
+                controller: _tabController,
+                children: [
+                  ListView.builder(
+                    itemCount: 3,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text("$index"),
+                      );
+                    },
+                  ),
+                  ListView.builder(
+                    itemCount: 3,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text("$index"),
+                      );
+                    },
+                  ),
+                  ListView.builder(
+                    itemCount: 3,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text("$index"),
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
